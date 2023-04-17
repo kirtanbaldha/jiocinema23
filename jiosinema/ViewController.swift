906d92e1c5ddf56168ea28c0184a68b318c5a9d6
kirtan baldha <kirtan9397@gmail.com>
2023-04-17 22:51:23 +0530

    Initial Commit

---
+++ jiosinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jiosinema
+//
+//  Created by R & W on 17/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
